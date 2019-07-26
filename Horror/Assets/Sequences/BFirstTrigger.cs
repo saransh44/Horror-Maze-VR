5215293c0763481f9098af0e0097fdbe8b8d4952
Saransh Gupta <srg537@nyu.edu>
2019-07-26 06:46:48 -0400

    Need to add Gun grabbing mechanics
    Added Table and Gun

---
+++ Horror/Assets/Sequences/BFirstTrigger.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.UI;
+
+public class BFirstTrigger : MonoBehaviour
+{
+
+    public GameObject ThePlayer;
+    public GameObject TextBox;
+    public GameObject TheMarker;
+
+    void OnTriggerEnter()
+    {
+        //ThePlayer.GetComponent<OVRPlayerController>().enabled = false;
+        StartCoroutine(ScenePlayer());
+    }
+
+    IEnumerator ScenePlayer()
+    {
+        TextBox.GetComponent<Text>().text = "Looks like a weapon on that table.";
+        yield return new WaitForSeconds(2.5f);
+        TextBox.GetComponent<Text>().text = "";
+        //ThePlayer.GetComponent<OVRPlayerController>().enabled = true;
+        TheMarker.SetActive(true);
+
+    }
+
+}
