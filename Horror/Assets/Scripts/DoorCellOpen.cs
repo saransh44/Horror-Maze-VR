f8e73a38b1e161a8a6736f5bfd0567ae083c85dc
Saransh Gupta <srg537@nyu.edu>
2019-07-26 05:03:42 -0400

    Need to Add Bench with Handgun

---
+++ Horror/Assets/Scripts/DoorCellOpen.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DoorCellOpen : MonoBehaviour
+{
+
+    public float TheDistance;
+    public GameObject ActionDisplay;
+    public GameObject ActionText;
+    public GameObject DoorHinge;
+    public AudioSource CreakSound;
+
+    void Update()
+    {
+        TheDistance = PlayerCasting.DistanceFromTarget;
+    }
+
+    void OnMouseOver()
+    {
+        if (TheDistance <= 2)
+        {
+            ActionDisplay.SetActive(true);
+            ActionText.SetActive(true);
+        }
+        if (Input.GetButtonDown("Oculus_CrossPlatform_Button2"))
+        {
+            if (TheDistance <= 2)
+            {
+                this.GetComponent<BoxCollider>().enabled = false;
+                ActionDisplay.SetActive(false);
+                ActionText.SetActive(false);
+                DoorHinge.GetComponent<Animation>().Play("FirstDoorOpenAnim");
+                CreakSound.Play();
+            }
+        }
+    }
+
+    void OnMouseExit()
+    {
+        ActionDisplay.SetActive(false);
+        ActionText.SetActive(false);
+    }
+}
---
+++ Horror/Assets/Sequences/AOpening.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.UI;
+
+public class AOpening : MonoBehaviour
+{
+
+    public GameObject ThePlayer;
+    public GameObject FadeScreenIn;
+    public GameObject TextBox;
+
+    void Start()
+    {
+        //ThePlayer.GetComponent<OVRPlayerController>().enabled = false;
+        StartCoroutine(ScenePlayer());
+    }
+
+    IEnumerator ScenePlayer()
+    {
+        yield return new WaitForSeconds(1.5f);
+        FadeScreenIn.SetActive(false);
+        TextBox.GetComponent<Text>().text = "I need to get out of here.";
+        yield return new WaitForSeconds(2);
+        TextBox.GetComponent<Text>().text = "";
+        //ThePlayer.GetComponent<OVRPlayerController>().enabled = true;
+
+    }
+
+}
